26880f2114b08378f7b274a3475454895f39ced2
家子智成 <>
2020-12-06 17:51:26 +0900

    人を100人並べる処理

---
+++ Assets/0_MyAssets/Scripts/Game/HumanManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanManager : MonoBehaviour
+{
+    [SerializeField] HumanController humanPrefab;
+    HumanController[] humanControllers;
+    void Start()
+    {
+        Vector3 startPos = new Vector3(-3.44f, 0.55f, 3.15f);
+        Vector3 pos = startPos;
+        float offset = 0.75f;
+        humanControllers = new HumanController[100];
+        int xCount = 0;
+        for (int i = 0; i < humanControllers.Length; i++)
+        {
+            humanControllers[i] = Instantiate(humanPrefab, pos, Quaternion.identity, transform);
+            xCount++;
+            pos.x += offset;
+            if (xCount == 10)
+            {
+                pos.x = startPos.x;
+                pos.z -= offset;
+                xCount = 0;
+            }
+        }
+    }
+
+
+}
