146fc03624f5a306169457b30ce51eb0f57af8d1
家子智成 <>
2020-12-09 00:01:42 +0900

    旗に当たったら跳ね返る処理

--- Assets/0_MyAssets/Scripts/Game/DozerController.cs
+++ Assets/0_MyAssets/Scripts/Game/DozerController.cs
@@ -1,35 +1,89 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
+
+public enum DozerState
+{
+    Control,
+    Back,
+}
 public class DozerController : MonoBehaviour
 {
     [SerializeField] Rigidbody rb;
     [SerializeField] DynamicJoystick joystick;
     Vector3 vel;
+    DozerState state;
+    float controlSpeed = 7f;
+
     void Start()
     {
-
+        state = DozerState.Control;
     }
 
 
     void Update()
     {
-        if (Input.GetMouseButton(0))
+
+        switch (state)
         {
-            vel = Vector3.zero;
-            vel.x = joystick.Horizontal;
-            vel.z = joystick.Vertical;
+            case DozerState.Control:
+                if (Input.GetMouseButton(0))
+                {
+                    vel = Vector3.zero;
+                    vel.x = joystick.Horizontal;
+                    vel.z = joystick.Vertical;
+                }
+                else
+                {
+                    vel = Vector3.zero;
+                }
+                break;
+            case DozerState.Back:
+                break;
+            default:
+                break;
         }
-        else
-        {
-            vel = Vector3.zero;
-        }
+
+
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = vel * 7f;
-        if (vel.sqrMagnitude > 0.1f) transform.forward = vel;
+
+        switch (state)
+        {
+            case DozerState.Control:
+                rb.velocity = vel * 7f;
+                if (vel.sqrMagnitude > 0.1f) transform.forward = vel;
+                break;
+            case DozerState.Back:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        var flag = other.gameObject.GetComponent<FlagController>();
+        if (flag == null) return;
+        state = DozerState.Back;
+        float backSpeed = 10f;
+        Vector3 dir = -transform.forward;
+        DOTween.To(() => backSpeed, (x) => backSpeed = x, 0, 0.7f)
+        .SetEase(Ease.Linear)
+        .OnUpdate(() =>
+        {
+            rb.velocity = dir * backSpeed;
+            transform.forward = -dir;
+        })
+        .OnComplete(() =>
+        {
+            state = DozerState.Control;
+        });
+
+
     }
 }
