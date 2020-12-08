787f065643f641e7b11e5446d8065d916809bbdf
家子智成 <>
2020-12-08 22:57:08 +0900

    ブルドーザーの操作

---
+++ Assets/0_MyAssets/Scripts/Game/DozerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DozerController : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    [SerializeField] DynamicJoystick joystick;
+    Vector3 vel;
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            vel = Vector3.zero;
+            vel.x = joystick.Horizontal;
+            vel.z = joystick.Vertical;
+        }
+        else
+        {
+            vel = Vector3.zero;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+
+
+        rb.velocity = vel * 7f;
+        if (vel.sqrMagnitude > 0.1f) transform.forward = vel;
+    }
+}
