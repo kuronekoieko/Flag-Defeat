ff5449bd325abd705cae23fc965958a109be3c37
家子智成 <>
2021-01-03 18:46:11 +0900

    押す用の棒作成

---
+++ Assets/0_MyAssets/Scripts/Game/PushEmAllToy/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PushEmAllToy
+{
+    public enum PlayerState
+    {
+        Control,
+        Back,
+    }
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] Rigidbody rb;
+        [SerializeField] DynamicJoystick joystick;
+        Vector3 vel;
+        PlayerState state;
+        float controlSpeed = 7f;
+
+        void Start()
+        {
+            state = PlayerState.Control;
+        }
+
+
+        void Update()
+        {
+
+            switch (state)
+            {
+                case PlayerState.Control:
+                    if (Input.GetMouseButton(0))
+                    {
+                        vel = Vector3.zero;
+                        vel.x = joystick.Horizontal;
+                        vel.z = joystick.Vertical;
+                    }
+                    else
+                    {
+                        vel = Vector3.zero;
+                    }
+                    break;
+                case PlayerState.Back:
+                    break;
+                default:
+                    break;
+            }
+
+
+        }
+
+        private void FixedUpdate()
+        {
+
+            switch (state)
+            {
+                case PlayerState.Control:
+                    rb.velocity = vel * 7f;
+                    if (vel.sqrMagnitude > 0.1f) transform.forward = vel;
+                    break;
+                case PlayerState.Back:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+}
