9ab129744221ab6fbe835e4d6ddf784753b6b2b8
家子智成 <>
2021-01-07 10:46:56 +0900

    女性キャラクターの顔の向き、アニメーション

--- Assets/0_MyAssets/Scripts/Game/PushEmAllToy/HandForwardIK.cs
+++ Assets/0_MyAssets/Scripts/Game/PushEmAllToy/HandForwardIK.cs
@@ -6,6 +6,7 @@ public class HandForwardIK : MonoBehaviour
 {
     [SerializeField] Transform leftHandTransform;
     [SerializeField] Transform rightHandTransform;
+    [SerializeField] Transform targetPosTf;
     private Animator animator;
 
     void Start()
@@ -16,13 +17,24 @@ public class HandForwardIK : MonoBehaviour
     void OnAnimatorIK()
     {
         //　左手、右手のIK設定
-        animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
-        animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
-        animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
-        animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
-        animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandTransform.position);
-        animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandTransform.rotation);
-        animator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandTransform.position);
-        animator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandTransform.rotation);
+        if (leftHandTransform)
+        {
+            animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+            animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+            animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+            animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
+            animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandTransform.position);
+            animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandTransform.rotation);
+            animator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandTransform.position);
+            animator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandTransform.rotation);
+        }
+
+        //注視方向
+        if (targetPosTf)
+        {
+            animator.SetLookAtWeight(1.0f, 0f, 1.0f, 0.0f, 0f);
+            animator.SetLookAtPosition(targetPosTf.position);
+        }
+
     }
 }
