e192278152333b0ecd662c1e3237066e89d04cce
家子智成 <>
2021-01-17 17:02:38 +0900

    攻撃失敗するバグ修正

--- Assets/0_MyAssets/Scripts/Game/FlagDefeat_1/AttackPartController.cs
+++ Assets/0_MyAssets/Scripts/Game/FlagDefeat_1/AttackPartController.cs
@@ -12,29 +12,39 @@ public class AttackPartController : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
         HitDozerBody(other);
         HitDozerAttack(other);
         HitFlag(other);
     }
 
-    void HitDozerBody(Collision other)
+    void HitDozerBody(Collider other)
     {
         var dozerBody = other.gameObject.GetComponent<DozerController1>();
+        if (dozerBody == null) return;
+        if (dozerBody == dozerController) return;
         dozerController.AttackBound();
         dozerBody.Broken();
     }
 
-    void HitDozerAttack(Collision other)
+    void HitDozerAttack(Collider other)
     {
         var dozerAttack = other.gameObject.GetComponent<AttackPartController>();
+        if (dozerAttack == null) return;
         dozerController.AttackBound();
     }
 
-    void HitFlag(Collision other)
+    void HitFlag(Collider other)
     {
         var flag = other.gameObject.GetComponent<FlagController>();
+        if (flag == null) return;
         dozerController.AttackBound();
         flag.Defeat(transform.forward);
+        Debug.Log("旗");
     }
 
 }
