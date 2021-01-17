95dd0d54cd538abe8c5b0b5c4e71071d2ba6ef53
家子智成 <>
2021-01-17 17:20:21 +0900

    プレイヤー操作とドーザー処理を分離

--- Assets/0_MyAssets/Scripts/Game/FlagDefeat_1/DozerController1.cs
+++ Assets/0_MyAssets/Scripts/Game/FlagDefeat_1/DozerController1.cs
@@ -2,61 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using System;
 
 public class DozerController1 : MonoBehaviour
 {
-    [SerializeField] Rigidbody rb;
-    [SerializeField] DynamicJoystick joystick;
-    Vector3 vel;
-    DozerState state;
+    public Rigidbody rb;
+
+    [NonSerialized] public DozerState state;
     float controlSpeed = 7f;
 
     void Start()
     {
         state = DozerState.Control;
-    }
-
-
-    void Update()
-    {
-
-        switch (state)
-        {
-            case DozerState.Control:
-                if (Input.GetMouseButton(0))
-                {
-                    vel = Vector3.zero;
-                    vel.x = joystick.Horizontal;
-                    vel.z = joystick.Vertical;
-                }
-                else
-                {
-                    vel = Vector3.zero;
-                }
-                break;
-            case DozerState.Back:
-                break;
-            default:
-                break;
-        }
-
-
-    }
-
-    private void FixedUpdate()
-    {
-
-        switch (state)
-        {
-            case DozerState.Control:
-                rb.velocity = vel * 7f;
-                if (vel.sqrMagnitude > 0.1f) transform.forward = vel;
-                break;
-            case DozerState.Back:
-                break;
-            default:
-                break;
-        }
     }
 
     public void AttackBound()
---
+++ Assets/0_MyAssets/Scripts/Game/FlagDefeat_1/PlayerDozerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDozerController : MonoBehaviour
+{
+    DynamicJoystick joystick;
+    DozerController1 dozerController;
+    Vector3 vel;
+
+    private void Awake()
+    {
+        joystick = FindObjectOfType<DynamicJoystick>();
+        dozerController = GetComponent<DozerController1>();
+    }
+
+    void Update()
+    {
+
+        switch (dozerController.state)
+        {
+            case DozerState.Control:
+                if (Input.GetMouseButton(0))
+                {
+                    vel = Vector3.zero;
+                    vel.x = joystick.Horizontal;
+                    vel.z = joystick.Vertical;
+                }
+                else
+                {
+                    vel = Vector3.zero;
+                }
+                dozerController.rb.velocity = vel * 7f;
+                if (vel.sqrMagnitude > 0.1f) transform.forward = vel;
+                break;
+            case DozerState.Back:
+                break;
+            default:
+                break;
+        }
+
+
+    }
+}
